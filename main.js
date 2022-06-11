3e502e7305cbce2b53070603bdee24e85b198397
mardanius <mariojeda.mg@gmail.com>
2022-06-11 12:13:19 -0400

    Added until '12. Prevent Infinite Loops with a Valid Terminal Condition'

--- main.js
+++ main.js
@@ -103,12 +103,18 @@ console.log(typeof three);
 
 
 
-/*-----
-Catch Misspelled Variable and Function Names
-The console.log() and typeof methods are the two primary ways to check intermediate values and types of program output. Now it's time to get into the common forms that bugs take. One syntax-level issue that fast typers can commiserate with is the humble spelling error.
+/*------------------------------------------
+4. Catch Misspelled Variable and Function Names
+--------------------------------------------
 
-Transposed, missing, or mis-capitalized characters in a variable or function name will have the browser looking for an object that doesn't exist - and complain in the form of a reference error. JavaScript variable and function names are case-sensitive.
+The console.log() and typeof methods are the two primary ways to check intermediate values and types of program output. 
+Now it's time to get into the common forms that bugs take. One syntax-level issue that fast typers can commiserate with is the 
+humble spelling error.
 
+Transposed, missing, or mis-capitalized characters in a variable or function name will have the browser looking for an object 
+that doesn't exist - and complain in the form of a reference error. JavaScript variable and function names are case-sensitive.
+
+--------------------------------------------------------------------------------------
 Fix the two spelling errors in the code so the netWorkingCapital calculation works. */
 
 let receivables = 10;
@@ -119,12 +125,19 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 
 
 
-/*---
-Catch Unclosed Parentheses, Brackets, Braces and Quotes
-Another syntax error to be aware of is that all opening parentheses, brackets, curly braces, and quotes have a closing pair. Forgetting a piece tends to happen when you're editing existing code and inserting items with one of the pair types. Also, take care when nesting code blocks into others, such as adding a callback function as an argument to a method.
+/*-----------------------------------------------------
+5. Catch Unclosed Parentheses, Brackets, Braces and Quotes
+-------------------------------------------------------
 
-One way to avoid this mistake is as soon as the opening character is typed, immediately include the closing match, then move the cursor back between them and continue coding. Fortunately, most modern code editors generate the second half of the pair automatically.
+Another syntax error to be aware of is that all opening parentheses, brackets, curly braces, and quotes have a closing pair. 
+Forgetting a piece tends to happen when you're editing existing code and inserting items with one of the pair types. Also, 
+take care when nesting code blocks into others, such as adding a callback function as an argument to a method.
 
+One way to avoid this mistake is as soon as the opening character is typed, immediately include the closing match, then move 
+the cursor back between them and continue coding. Fortunately, most modern code editors generate the second half of the pair 
+automatically.
+
+--------------------------------------
 Fix the two pair errors in the code.*/
 
 let myArray = [1, 2, 3];
@@ -134,11 +147,15 @@ console.log(`Sum of array values is: ${arraySum}`);
 
 
 
-/*---
-Catch Mixed Usage of Single and Double Quotes
-JavaScript allows the use of both single (') and double (") quotes to declare a string. Deciding which one to use generally comes down to personal preference, with some exceptions.
+/*-------------------------------------------
+6. Catch Mixed Usage of Single and Double Quotes
+---------------------------------------------
 
-Having two choices is great when a string has contractions or another piece of text that's in quotes. Just be careful that you don't close the string too early, which causes a syntax error.
+JavaScript allows the use of both single (') and double (") quotes to declare a string. Deciding which one to use generally 
+comes down to personal preference, with some exceptions.
+
+Having two choices is great when a string has contractions or another piece of text that's in quotes. Just be careful that you 
+don't close the string too early, which causes a syntax error.
 
 Here are some examples of mixing quotes:
 
@@ -147,10 +164,14 @@ const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quot
 const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it.';
 The first two are correct, but the third is incorrect.
 
-Of course, it is okay to use only one style of quotes. You can escape the quotes inside the string by using the backslash (\) escape character:
+Of course, it is okay to use only one style of quotes. You can escape the quotes inside the string by using the backslash (\) 
+escape character:
 
 const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
-Fix the string so it either uses different quotes for the href value, or escape the existing ones. Keep the double quote marks around the entire string. */
+
+--------------------------------------------------------------------------------------------------
+Fix the string so it either uses different quotes for the href value, or escape the existing ones. 
+Keep the double quote marks around the entire string. */
 
 let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
 console.log(innerHtml);
@@ -159,7 +180,7 @@ console.log(innerHtml);
 
 
 /*-----------------------------------------------------------
-Catch Use of Assignment Operator Instead of Equality Operator
+7. Catch Use of Assignment Operator Instead of Equality Operator
 -------------------------------------------------------------
 
 Branching programs, i.e. ones that do different things if certain conditions are met, rely on if, else if, and else statements 
@@ -182,8 +203,8 @@ if (x = y) {
 } else {
 
 }
-In this example, the code block within the if statement will run for any value of y, unless y is falsy. The else block, which 
-we expect to run here, will not actually run.
+In this example, the code block within the if statement will run for any value of y, unless y is falsy. The else block, 
+which we expect to run here, will not actually run.
 
 -----------------------------------------------------------------------------------------------------------
 Fix the condition so the program runs the right branch, and the appropriate value is assigned to result. */
@@ -199,3 +220,178 @@ if(x == y) {
 }
 
 console.log(result);
+
+
+
+
+/*--------------------------------------------------------------
+8. Catch Missing Open and Closing Parenthesis After a Function Call
+----------------------------------------------------------------
+
+When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses 
+when calling it. Often times the result of a function call is saved in a variable for other use in your code. This error can 
+be detected by logging variable values (or their types) to the console and seeing that one is set to a function reference, 
+instead of the expected value the function returns.
+
+The variables in the following example are different:
+
+function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
+Here varOne is the function myFunction, and varTwo is the string You rock!.
+
+Fix the code so the variable result is set to the value returned from calling the function getNine. */
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+
+
+
+/*---------------------------------------------------------------
+9. Catch Arguments Passed in the Wrong Order When Calling a Function
+-----------------------------------------------------------------
+
+Continuing the discussion on calling functions, the next bug to watch out for is when a function's arguments are supplied in 
+the incorrect order. If the arguments are different types, such as a function expecting an array and an integer, this will 
+likely throw a runtime error. If the arguments are the same type (all integers, for example), then the logic of the code won't 
+make sense. Make sure to supply all required arguments, in the proper order to avoid these issues.
+
+------------------------------------------------------------------------------------------------------------------------------
+The function raiseToPower raises a base to an exponent. Unfortunately, it's not called properly - fix the code so the value of 
+power is the expected 8. */
+
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+
+
+
+/*-----------------------------------------
+10. Catch Off By One Errors When Using Indexing
+-------------------------------------------
+
+Off by one errors (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array 
+(to slice or access a segment), or when looping over the indices of them. JavaScript indexing starts at zero, not one, 
+which means the last index is always one less than the length of the item. If you try to access an index equal to the length, 
+the program may throw an "index out of range" reference error or print undefined.
+
+When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand 
+if they are inclusive (the item at the given index is part of what's returned) or not. Here are some examples of off by one 
+errors:
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  console.log(alphabet[k]);
+}
+The first example here loops one too many times, and the second loops one too few times (missing the first index, 0). The third example is correct.
+
+-------------------------------------------------------------------------------------------------------------------
+Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console. */
+
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+
+
+
+/*-----------------------------------------------------
+11. Use Caution When Reinitializing Variables Inside a Loop
+-------------------------------------------------------
+
+Sometimes it's necessary to save information, increment counters, or re-set variables within a loop. A potential issue is when 
+variables either should be reinitialized, and aren't, or vice versa. This is particularly dangerous if you accidentally reset 
+the variable being used for the terminal condition, causing an infinite loop.
+
+Printing variable values with each cycle of your loop by using console.log() can uncover buggy behavior related to resetting, 
+or failing to reset a variable.
+
+------------------------------------------------------------------------------------------------------------------------
+The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, 
+it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) 
+in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]]. */
+
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+      let row = [];
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+
+
+
+
+/*----------------------------------------------------
+12. Prevent Infinite Loops with a Valid Terminal Condition
+------------------------------------------------------
+
+The final topic is the dreaded infinite loop. Loops are great tools when you need your program to run a code block a certain 
+number of times or until a condition is met, but they need a terminal condition that ends the looping. 
+Infinite loops are likely to freeze or crash the browser, and cause general program execution mayhem, which no one wants.
+
+There was an example of an infinite loop in the introduction to this section - it had no terminal condition to break out of 
+the while loop inside loopy(). Do NOT call this function!
+
+function loopy() {
+  while(true) {
+    console.log("Hello, world!");
+  }
+}
+It's the programmer's job to ensure that the terminal condition, which tells the program when to break out of the loop code, 
+is eventually reached. One error is incrementing or decrementing a counter variable in the wrong direction from the terminal 
+condition. Another one is accidentally resetting a counter or index variable within the loop code, instead of incrementing or 
+decrementing it.
+
+------------------------------------------------------------------------------------------------------------------
+The myFunc() function contains an infinite loop because the terminal condition i != 4 will never evaluate to false 
+(and break the looping) - i will increment by 2 each pass, and jump right over 4 since i is odd to start. 
+Fix the comparison operator in the terminal condition so the loop only runs for i less than or equal to 4. */
+
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
