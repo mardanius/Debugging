0b6fb33143e14cc8976c38ddd62a49e71554e71a
mardanius <mariojeda.mg@gmail.com>
2022-06-11 10:28:08 -0400

    Catch Use of Assignment Operator Instead of Equality Operator

--- main.js
+++ main.js
@@ -151,3 +151,51 @@ Of course, it is okay to use only one style of quotes. You can escape the quotes
 
 const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
 Fix the string so it either uses different quotes for the href value, or escape the existing ones. Keep the double quote marks around the entire string. */
+
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+
+
+
+/*-----------------------------------------------------------
+Catch Use of Assignment Operator Instead of Equality Operator
+-------------------------------------------------------------
+
+Branching programs, i.e. ones that do different things if certain conditions are met, rely on if, else if, and else statements 
+in JavaScript. The condition sometimes takes the form of testing whether a result is equal to a value.
+
+This logic is spoken (in English, at least) as "if x equals y, then ..." which can literally translate into code using the =, 
+or assignment operator. This leads to unexpected control flow in your program.
+
+As covered in previous challenges, the assignment operator (=) in JavaScript assigns a value to a variable name. 
+And the == and === operators check for equality (the triple === tests for strict equality, meaning both value and type are 
+    the same).
+
+The code below assigns x to be 2, which evaluates as true. Almost every value on its own in JavaScript evaluates to true, 
+except what are known as the "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null.
+
+let x = 1;
+let y = 2;
+if (x = y) {
+
+} else {
+
+}
+In this example, the code block within the if statement will run for any value of y, unless y is falsy. The else block, which 
+we expect to run here, will not actually run.
+
+-----------------------------------------------------------------------------------------------------------
+Fix the condition so the program runs the right branch, and the appropriate value is assigned to result. */
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
